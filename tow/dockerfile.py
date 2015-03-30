4ba6293a862592217d700705bd20e2314796494b
Aleksei Kornev <akornev@griddynamics.com>
2015-03-30 23:06:31 +0300

    Add find_entrypoint_cmd method and cmd patch method

--- tow/dockerfile.py
+++ tow/dockerfile.py
@@ -28,13 +28,13 @@ class Dockerfile(object):
             equal_index = re.search("\w+=", env_line)
             env_name = equal_index.group().split("=")[0]
             env_line = env_line[len(equal_index.group()):]
-            if env_line.startswith("\""): # if starts from " parse text between quotes
+            if env_line.startswith("\""):  # if starts from " parse text between quotes
                 env_var = re.search("([\"'])(?:(?=(\\\\?))\\2.)*?\\1", env_line).group()
                 env_line = env_line[len(env_var):]
-                env_var = re.sub(r'^"|"$', '', env_var) # remove around quotes
+                env_var = re.sub(r'^"|"$', '', env_var)  # remove around quotes
             else:
-                env_var_match = re.search(".*([^\\\\]\\s)", env_line) # read until space
-                if not env_var_match: # if space doesn't found that mean that it's the last variable in string
+                env_var_match = re.search(".*([^\\\\]\\s)", env_line)  # read until space
+                if not env_var_match:  # if space doesn't found that mean that it's the last variable in string
                     env_var = env_line
                 else:
                     env_var = env_var_match.group()
@@ -46,6 +46,14 @@ class Dockerfile(object):
 
         is_multiline_envs = env_line.endswith("\\")
         return (result, is_multiline_envs)
+
+    def _parse_exec_line(self, exec_line):
+        command = exec_line[exec_line.find(" ") + 1:]
+        if command.startswith("[") and command.endswith("]"):  # Handle array command
+            command = command[1:-1]
+            return " ".join([sh.strip()[1:-1] for sh in command.split(",")])
+        else:  # It's just shell notation
+            return command.strip()
 
     def envs(self):
         if not self.__envs:
@@ -69,19 +77,47 @@ class Dockerfile(object):
                 self.__envs.update(env_vars)
         return self.__envs
 
-
     def add_copy(self, mapping):
-        copy_block = ["COPY %s %s" % (src, dst) for (src, dst) in mapping ]
+        copy_block = ["COPY %s %s" % (src, dst) for (src, dst) in mapping]
         position = len(self.__dockerfile)
 
         for i, dockerfile_line in enumerate(self.__dockerfile):
             if dockerfile_line.startswith("CMD") or dockerfile_line.startswith("ENTRYPOINT"):
-               position = i
-               break
+                position = i
+                break
 
         self.__dockerfile = self.__dockerfile[:position] + copy_block + self.__dockerfile[position:]
 
+    def find_entrypoint_or_cmd(self):
+        """ This command find entrypoint or cmd blocks and
+        return them in shell style
+        """
+        cmd = None
+        entrypoint = None
+        for dockerfile_line in self.__dockerfile:
+            if dockerfile_line.startswith("CMD"):
+                cmd = dockerfile_line
+            elif dockerfile_line.startswith("ENTRYPOINT"):
+                entrypoint = dockerfile_line
+
+        result = ""
+        for exec_line in [entrypoint, cmd]:
+            if exec_line:
+                result = result + self._parse_exec_line(exec_line)
+
+        return result if result else None
+
+    def replace_entrypoint_or_cmd_by_tow(self, cmd):
+        self.__dockerfile = [dockerfile_line for dockerfile_line in self.__dockerfile
+                             if dockerfile_line.startswith("CMD") or dockerfile_line.startswith("ENTRYPOINT")]
+
+        position = len(self.__dockerfile)
+        for i, dockerfile_line in enumerate(self.__dockerfile):
+            if dockerfile_line.startswith("CMD"):
+                position = i
+                break
+        self.__dockerfile = self.__dockerfile[:position] + cmd + self.__dockerfile[position:]
 
     def save(self, dockerfile_path):
         with open(dockerfile_path, "w+") as df:
-           df.write("\n".join(self.__dockerfile))
+            df.write("\n".join(self.__dockerfile))
